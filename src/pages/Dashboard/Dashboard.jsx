b182f4e51b0ea589f7d196e1619c532a91d0b8e3
avel40m <avel40m@gmail.com>
2023-09-09 19:30:01 -0300

    detalles parte dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,41 @@
+import Cookies from "js-cookie"
+import { useEffect, useState } from "react"
+import { userGuideVehicle } from "../../services/user";
+
 export const Dashboard = () => {
+  const cookie = Cookies.get();
+  const [users, setUsers] = useState(null)
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await userGuideVehicle(cookie.token);
+      setUsers(response);
+    }
+    fetchUser()
+  }, [])
+  console.log(users);
   return (
     <main className="main-content">
-      <h2>Dashboard</h2>
+      <div className="datos-personales">
+        <h2>Datos Personales</h2>
+        <p>Nombre: {users?.nombre}</p>
+        <p>Apellido: {users?.apellido}</p>
+        <p>Email: {users?.email}</p>
+        <p>D.N.I.: {users?.dni}</p>
+      </div>
+      <div className="datos-personales">
+        <h2>Datos del guia</h2>
+        <p>Carnet: {users?.guia.carnet}</p>
+        <p>Cedula: {users?.guia.cedula}</p>
+        <p>Licencia: {users?.guia.licencia}</p>
+      </div>
+      <div className="datos-personales">
+        <h2>Vehiculos</h2>
+        {users?.vehiculos.map((vehiculo) => (
+          <li key={vehiculo.id}>
+            Asientos: {vehiculo.asientos}, Tipo: {vehiculo.tipo}
+          </li>
+        ))}
+      </div>
     </main>
   )
 }
--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,7 @@
 const LOGIN_URL = 'http://localhost:3000/api/signin'
 const REGISTER_URL = 'http://localhost:3000/api/signup'
 const USER_UPDATE_URL = 'http://localhost:3000/api/usuarios'
+const USER_GUIDE_URL = 'http://localhost:3000/api/usuarios/guia'
 
 export const login = async (data) => {
   try {
@@ -43,6 +44,21 @@ export const updateUser = async (userId,datos,token) => {
       body: JSON.stringify(datos)
     });
     return response;
+  } catch (error) {
+    return error;
+  }
+}
+
+export const userGuideVehicle = async (token) => {
+  try {
+    const response = await fetch(`${USER_GUIDE_URL}`,{
+      method: 'GET',
+      headers: {
+        'Content-type':'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    return response.json();
   } catch (error) {
     return error;
   }
